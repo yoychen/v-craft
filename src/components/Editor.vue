5ebac2b3cccd0b3f0e593359827797146d551552
yoychen <yui12327@gmail.com>
2020-03-14 18:10:27 +0000

    Implement the base of Editor component

---
+++ src/components/Editor.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :is="component">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+import Editor from '../core/Editor';
+
+export default {
+  props: {
+    component: [Object, String],
+    resolverMap: Object,
+  },
+  data() {
+    return {
+      editor: new Editor([], this.resolverMap),
+    };
+  },
+  provide() {
+    return {
+      editor: this.editor,
+    };
+  },
+};
+</script>
---
+++ tests/unit/components/Editor.spec.js
@@ -0,0 +1,44 @@
+import { shallowMount } from '@vue/test-utils';
+import EditorComponent from '@/components/Editor.vue';
+import Editor from '@/core/Editor';
+
+jest.mock('@/core/Editor.js');
+
+function shallowMountEditor(props, defaultSlot = []) {
+  return shallowMount(EditorComponent, {
+    propsData: props,
+    slots: {
+      default: defaultSlot,
+    },
+  });
+}
+
+describe('initialization', () => {
+  it('instantiates Editor object', () => {
+    Editor.mock.instances = [];
+
+    shallowMountEditor({
+      component: 'div',
+    });
+
+    expect(Editor.mock.instances.length).toBe(1);
+  });
+});
+
+describe('initialization', () => {
+  it('renders root element by component props', () => {
+    const wrapper = shallowMountEditor({
+      component: 'div',
+    });
+    expect(wrapper.contains('div')).toBe(true);
+  });
+
+  it('renders default slots into the root element', () => {
+    const wrapper = shallowMountEditor({
+      component: 'div',
+    }, [
+      '<span>content</span>',
+    ]);
+    expect(wrapper.contains('div span')).toBe(true);
+  });
+});
