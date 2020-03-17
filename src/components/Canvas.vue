9e7e6e61cf414a0e39e0b8d8b05a28ac24dccde3
yoychen <yui12327@gmail.com>
2020-03-17 15:29:41 +0000

    Implement Canvas component

---
+++ src/components/Canvas.vue
@@ -0,0 +1,21 @@
+<template>
+  <component :is="editor.findResolver(component)">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    component: String,
+  },
+  inject: [
+    'editor',
+  ],
+  methods: {
+    getCraftOption() {
+      return this.component.craft || {};
+    },
+  },
+};
+</script>
---
+++ tests/unit/components/Canvas.spec.js
@@ -0,0 +1,41 @@
+import { shallowMount } from '@vue/test-utils';
+import CanvasComponent from '@/components/Canvas.vue';
+import {
+  createFakeEditor,
+} from '../helpers';
+
+function shallowMountCanvas(props = {}, defaultSlot = [], mocks) {
+  const defaultMocks = {
+    editor: createFakeEditor(),
+  };
+
+  return shallowMount(CanvasComponent, {
+    propsData: props,
+    slots: {
+      default: defaultSlot,
+    },
+    mocks: mocks || defaultMocks,
+  });
+}
+
+describe('view', () => {
+  it('renders root element by the result of findResolver() of editor', () => {
+    const editor = createFakeEditor();
+    editor.findResolver = () => 'header';
+
+    const wrapper = shallowMountCanvas({}, [], { editor });
+
+    expect(wrapper.contains('header')).toBe(true);
+  });
+
+  it('renders default slots into the root element', () => {
+    const editor = createFakeEditor();
+    editor.findResolver = () => 'header';
+
+    const wrapper = shallowMountCanvas({}, [
+      '<span>content</span>',
+    ], { editor });
+
+    expect(wrapper.contains('header span')).toBe(true);
+  });
+});
