0879d0743c3bda5f0729c2c8755fe01993f686ee
yoychen <yui12327@gmail.com>
2020-03-28 11:28:16 +0000

    Implement export/import functionality

--- src/example/components/Navbar.vue
+++ src/example/components/Navbar.vue
@@ -3,15 +3,47 @@
     <span class="brand">V-Craft</span>
     <div class="actions">
       <a href="#">Github</a>
-      <a href="#">Import</a>
-      <a href="#">Export</a>
+      <a href="#" @click.prevent="toggleImportDialog">Import</a>
+      <a href="#" @click.prevent="toggleExportDialog">Export</a>
       <el-switch
         :value="enabled"
         @input="updateState"
         active-color="#13ce66"
-        inactive-color="#ff4949">
+        inactive-color="gray">
       </el-switch>
     </div>
+
+    <el-dialog
+      title="Export"
+      :visible.sync="showExportDialog"
+      :modal="false"
+      width="30%"
+    >
+      <div v-if="showExportDialog" class="disable-select">
+        <textarea
+          ref="exportTextarea"
+          :rows="8"
+          :value="editor.export()"
+        />
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button size="medium" @click="toggleExportDialog">Close</el-button>
+        <el-button size="medium" type="primary" @click="copyExport">Copy</el-button>
+      </span>
+    </el-dialog>
+
+    <el-dialog
+      title="Import"
+      :visible.sync="showImportDialog"
+      :modal="false"
+      width="30%"
+    >
+      <textarea :rows="8" v-model="importData" />
+      <span slot="footer" class="dialog-footer">
+        <el-button size="medium" @click="toggleImportDialog">Close</el-button>
+        <el-button size="medium" type="primary" @click="doImport">Import</el-button>
+      </span>
+    </el-dialog>
   </nav>
 </template>
 
@@ -20,6 +52,13 @@ export default {
   inject: [
     'editor',
   ],
+  data() {
+    return {
+      showExportDialog: false,
+      showImportDialog: false,
+      importData: '',
+    };
+  },
   computed: {
     enabled() {
       return this.editor.enabled;
@@ -31,6 +70,27 @@ export default {
         this.editor.enable();
       } else {
         this.editor.disable();
+      }
+    },
+    toggleExportDialog() {
+      this.showExportDialog = !this.showExportDialog;
+    },
+    toggleImportDialog() {
+      this.showImportDialog = !this.showImportDialog;
+    },
+    copyExport() {
+      this.$refs.exportTextarea.select();
+      document.execCommand('Copy');
+      this.$message.success('Copied!');
+    },
+    doImport() {
+      try {
+        this.editor.import(this.importData);
+        this.$message.success('Imported!');
+        this.importData = '';
+        this.toggleImportDialog();
+      } catch ({ message }) {
+        this.$message.error(message);
       }
     },
   },
@@ -70,6 +130,31 @@ export default {
     &:hover {
       color: $color-black;
     }
+  }
+}
+
+.disable-select {
+  position: relative;
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+}
+</style>
+
+<style lang="scss">
+.navbar .el-dialog__body {
+  padding: 0 24px 12px;
+
+  textarea {
+    display: block;
+    width: 100%;
+    border-radius: 3px;
+    color: rgba(0, 0, 0, 0.7);
   }
 }
 </style>
