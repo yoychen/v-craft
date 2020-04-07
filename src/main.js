d919d0d5b153edcd38a0046abd79fa47a5b2b63b
yoychen <yui12327@gmail.com>
2020-04-07 07:08:27 +0000

    Externalize the dependencies to decrease npm package size
    Use webpack `externals` config

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,14 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/v-craft/'
     : '/',
+  chainWebpack: (config) => {
+    // When the argument `--target` is 'lib'
+    if (process.argv[4] === 'lib') {
+      config.externals([
+        'lodash/kebabCase',
+        'lodash/cloneDeep',
+        'uuid',
+      ]);
+    }
+  },
 };
